54fb27b922c2ccd92b14b660c98c480995ff6b18
m19182-server <mateoamadoares@gmail.com>
2024-10-09 11:28:28 +0200

    ffmpeg win linux

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import numpy as np
-import os 
-
+import subprocess
+import os
 from classes.Simulation import Simulation
 from utils.get_info import get_data
 
@@ -19,8 +19,10 @@ def main():
     print('Creating animation...')
     anim = sim.animate_cv2(output_folder='data/animation_frames')
 
-    os.system(f'ffmpeg -framerate 30 -i data/animation_frames/frame_%04d.png -vf "scale=800:-1" {'multi_person_movement.gif'}')
-    print(f"Animation saved as '{'multi_person_movement.gif'}'")
+    output_file = 'multi_person_movement.gif'
+    subprocess.run(['ffmpeg', '-framerate', '30', '-i', 'data/animation_frames/frame_%04d.png', 
+                    '-vf', 'scale=800:-1', output_file], check=True)
+    print(f"Animation saved as '{output_file}'")
 
     for file_path in (os.path.join('data/animation_frames', f) for f in os.listdir('data/animation_frames')):
         os.remove(file_path)
