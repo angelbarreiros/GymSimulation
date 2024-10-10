fe8fa7a8ccec0841c2425c7783f7e58d37a5c8a9
m19182-server <mateoamadoares@gmail.com>
2024-10-10 18:13:41 +0200

    choose machine or random

--- classes/Map.py
+++ classes/Map.py
@@ -11,16 +11,23 @@ class Area:
         self.floor = floor
         self.type = type
         self.machines = machines
+        self.ocuppiedMachines = [False] * len(machines)
 
     def getPointInside(self):
-        # random point
-        # x, y, w, h = cv2.boundingRect(self.points)
-        # while True:
-        #     random_point = (np.random.randint(x, x + w), np.random.randint(y, y + h))
-        #     if cv2.pointPolygonTest(self.points, random_point, False) >= 0:
-        #         return random_point
-        #random machine point  
-        return np.random.choice(self.machines)
+
+        if self.machines: # If area has machines, choose a random machine that isnt occupied
+            available_machines = [i for i, occupied in enumerate(self.ocuppiedMachines) if not occupied]
+            if available_machines:
+                chosen_machine = np.random.choice(available_machines)
+            self.ocuppiedMachines[chosen_machine] = True
+            return self.machines[chosen_machine]
+        else: # If area has no machines, choose a random point inside the area
+            x, y, w, h = cv2.boundingRect(self.points)
+            while True:
+                random_point = (np.random.randint(x, x + w), np.random.randint(y, y + h))
+                if cv2.pointPolygonTest(self.points, random_point, False) >= 0:
+                    return random_point
+
 
     
     def contains_point(self, point_x, point_y):
