9fd8efafe09d45dc21f384fe7a0ba84ac1090989
Xan8 <49766374+Xan8@users.noreply.github.com>
2024-10-17 12:36:36 +0200

    xan puto

--- data/formated_data/get_final_data.py
+++ data/formated_data/get_final_data.py
@@ -1,17 +1,20 @@
 import get_classes_data as gc
 import get_insout_data as io
 import get_zones_data as gz
-
+import os
 
 
 if __name__ == "__main__":
+    if not os.path.exists('data/formated_data/zones'):
+        os.mkdir('data/formated_data/zones')
     gz.excel_to_json("data/excel/zonas-sept-zgz.xlsx")
     try:
         gc.transform_json("data/excel/clases_total.json")
+        io.process_json_file("data/excel/entradas_total.json")
         
     except Exception :
         pass
-    io.process_json_file("data/excel/entradas_total.json")
+    
         
     
     
