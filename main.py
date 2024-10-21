cea37814b2f5b81d8d823bd259c1c24ebeb17f58
angelbarreiros <angel.barreiros@udc.es>
2024-10-21 14:25:49 +0200

    Add september clasess data

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ import cProfile
 from classes.Simulation import Simulation
 
 TOTAL_FRAMES = 600
+<<<<<<< Updated upstream
 HOURS = [11, 12, 13, 14]
+=======
+HOURS = [7, 8, 9,10,11,12,13,14,15,16,17,18,19,20,21,22]
+>>>>>>> Stashed changes
 def main():
     np.random.seed(123)
     sim = Simulation()
